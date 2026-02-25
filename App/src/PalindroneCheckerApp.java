6d626d9fd59024dcd241ca91550318eeb3123564
Dheeraj2521 <PolemDheeraj20@gmail.com>
2026-02-25 16:18:49 +0530

    [Dheeraj2521] Added [UC11-Object-Oriented]

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,33 +1,40 @@
 import java.util.Scanner;
 
-public class PalindroneCheckerApp {
+class PalindromeChecker {
 
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to Palindrome Checker App - Usecase 10");
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a sentence: ");
-        String input = sc.nextLine();
-
-        // Normalize string: remove spaces & convert to lowercase
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+    public boolean checkPalindrome(String input) {
 
         int start = 0;
-        int end = normalized.length() - 1;
-        boolean isPalindrome = true;
+        int end = input.length() - 1;
 
         while (start < end) {
-            if (normalized.charAt(start) != normalized.charAt(end)) {
-                isPalindrome = false;
-                break;
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
             }
             start++;
             end--;
         }
+        return true;
+    }
+}
 
-        if (isPalindrome) {
+public class PalindroneCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Palindrome Checker App - Usecase 11");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String input = sc.nextLine();
+
+        // Using OOPS service class
+        PalindromeChecker checker = new PalindromeChecker();
+
+        boolean result = checker.checkPalindrome(input);
+
+        if (result) {
             System.out.println("Palindrome");
         } else {
             System.out.println("Not Palindrome");
